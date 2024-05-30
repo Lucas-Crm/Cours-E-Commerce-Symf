68767d6e586bf76ce9b42b2a17b48a57f99c7b77
Lucas-Crm <cormy90@gmail.com>
2024-05-30 22:30:34 +0200

    feat(marque) Add Create and Delete for marque

--- src/Controller/Backend/MarqueController.php
+++ src/Controller/Backend/MarqueController.php
@@ -7,6 +7,7 @@ use App\Form\MarqueType;
 use App\Repository\MarqueRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -31,7 +32,7 @@ class MarqueController extends AbstractController
     }
 
     #[Route('/{id}/update', name: '.update', methods: ['GET', 'POST'])]
-    public function update(?Marque $marque, Request $request)
+    public function update(?Marque $marque, Request $request) : RedirectResponse | Response
     {
             if(!$marque) {
                 $this->addFlash('error', 'Aucune marque n\'as ete trouver');
@@ -58,7 +59,75 @@ class MarqueController extends AbstractController
 
         }
 
+    #[Route('/{id}/delete', name: '.delete', methods: ['POST'])]
+    public function delete(?Marque $marque, Request $request): RedirectResponse
+    {
+
+        if(!$marque){
+            $this->addFlash('error', 'Aucune marque trouver');
+            return $this->redirectToRoute('admin.marque.index');
+        }
 
 
+        if($this->isCsrfTokenValid('delete'. $marque->getId(), $request->get('token'))){
+            $this->em->remove($marque);
+            $this->em->flush();
+
+            $this->addFlash('success', 'Une marque a ete supprimer');
+
+            return $this->redirectToRoute('admin.marque.index');
+        } else if (!$this->isCsrfTokenValid('delete'. $marque->getId(), $request->get('token'))){
+            $this->addFlash('error', 'Le token csrf n\as pas ete valide');
+        }
+
+            return $this->redirectToRoute('admin.marque.index');
+
+
+
+    }
+
+//    #[Route('/create', name: '.create', methods: ['GET', 'POST'])]
+//    public function create(Request $request): RedirectResponse | Response
+//    {
+//
+//        $marque = new Marque();
+//        $form = $this->createForm(MarqueType::class, $marque);
+//        $form->handleRequest($request);
+//
+//        if($form->isSubmitted() && $form->isValid()){
+//            $this->em->persist($marque);
+//            $this->em->flush();
+//
+//            $this->addFlash('success', 'Une marque a bien ete cree');
+//
+//            return $this->redirectToRoute('admin.marque.index');
+//        }
+//
+//        return $this->render('Backend/marque/index.html.twig', [
+//            'form'=>$form
+//        ]);
+//
+//
+//    }
+    #[Route('/create', name: '.create', methods: ['GET', 'POST'])]
+    public function create(Request $request): RedirectResponse | Response
+    {
+        $marque = new Marque();
+        $form = $this->createForm(MarqueType::class, $marque);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $this->em->persist($marque);
+            $this->em->flush();
+
+            $this->addFlash('success', 'Une marque a bien ete cree');
+
+            return $this->redirectToRoute('admin.marque.index');
+        }
+
+        return $this->render('Backend/marque/create.html.twig', [
+            'form' => $form
+        ]);
+    }
 
 }
